faafcd52e7c36c8dfd52cc83758d992e48737cc5
Ali Akhtari <Aliakhtari78@hotmail.com>
2023-01-02 18:31:31 +0330

    Update project

--- DormManagementSystem/settings.py
+++ DormManagementSystem/settings.py
@@ -102,11 +102,12 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/4.1/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'fa-ir'
 
 TIME_ZONE = 'Asia/Tehran'
 
-USE_I18N = False
+USE_L10N = True
+USE_I18N = True
 
 USE_TZ = True
 
--- Panel/models.py
+++ Panel/models.py
@@ -6,7 +6,7 @@ from django.db import models
 
 # Create your models here.
 class Setting(models.Model):
-    allow_new_user = models.BooleanField(default=True, blank=False, null=False)
+    allow_new_user = models.BooleanField(default=True, blank=False, null=False, verbose_name='وضعیت ثبت نام')
 
     def __str__(self):
         if self.allow_new_user:
@@ -14,28 +14,44 @@ class Setting(models.Model):
         else:
             return 'تکمیل ثبت نام'
 
+    class Meta:
+        verbose_name = "تنظیمات سایت"
+        verbose_name_plural = "تنظیمات سایت"
+
 
 class Dorm(models.Model):
-    title = models.CharField(max_length=64, blank=False, null=False, default='خوابگاه')
-    status = models.BooleanField(default=True, null=False, blank=False)
-    code = models.CharField(max_length=64, blank=False, null=False, unique=True)
-    created_date = models.DateTimeField(auto_now_add=True, blank=False, null=False, editable=False)
-    sex = models.BooleanField(default=False, blank=True, null=True)  # True for male, False for female
+    title = models.CharField(max_length=64, blank=False, null=False, default='خوابگاه', verbose_name='عنوان')
+    status = models.BooleanField(default=True, null=False, blank=False, verbose_name='وضعیت')
+    code = models.CharField(max_length=64, blank=False, null=False, unique=True, verbose_name='کد')
+    created_date = models.DateTimeField(auto_now_add=True, blank=False, null=False, editable=False,
+                                        verbose_name='زمان ایجاد')
+    sex = models.BooleanField(default=False, blank=True, null=True,
+                              verbose_name='جنسیت')  # True for male, False for female
 
     def __str__(self):
         return f'{self.title}, {self.code}'
 
+    class Meta:
+        verbose_name = "خوابگاه"
+        verbose_name_plural = "خوابگاه ها"
+
 
 class Room(models.Model):
-    title = models.CharField(max_length=64, blank=False, null=False, default='خوابگاه')
-    status = models.BooleanField(default=True, null=False, blank=False)
-    code = models.CharField(max_length=64, blank=False, null=False, unique=True)
-    dorm = models.ForeignKey(to=Dorm, on_delete=models.PROTECT, blank=False, null=False, related_name='+')
-    capacity = models.SmallIntegerField(blank=False, null=False, default=1)
-    created_date = models.DateTimeField(auto_now_add=True, blank=False, null=False, editable=False)
+    title = models.CharField(max_length=64, blank=False, null=False, default='خوابگاه', verbose_name='عنوان')
+    status = models.BooleanField(default=True, null=False, blank=False, verbose_name='وضعیت')
+    code = models.CharField(max_length=64, blank=False, null=False, unique=True, verbose_name='کد')
+    dorm = models.ForeignKey(to=Dorm, on_delete=models.PROTECT, blank=False, null=False, related_name='+',
+                             verbose_name='خوابگاه مورد نظر')
+    capacity = models.SmallIntegerField(blank=False, null=False, default=1, verbose_name='ظرفیت')
+    created_date = models.DateTimeField(auto_now_add=True, blank=False, null=False, editable=False,
+                                        verbose_name='زمان ایجاد')
 
     def __str__(self):
         return f'{self.title}, {self.code}, {self.dorm}'
+
+    class Meta:
+        verbose_name = "اتاق"
+        verbose_name_plural = "اتاق ها"
 
 
 class UserProfile(models.Model):
@@ -45,22 +61,30 @@ class UserProfile(models.Model):
         ('دکترا', 'دکترا'),
         ('مدیر', 'مدیر'),
     )
-    registration_date = models.DateTimeField(blank=True, null=True, auto_now_add=True)
-    user_instance = models.ForeignKey(to=User, on_delete=models.PROTECT, blank=False, null=False, related_name='+')
-    student_number = models.CharField(max_length=64, blank=False, null=False)
-    sex = models.BooleanField(default=False, blank=True, null=True)  # True for male, False for female
-    room = models.ForeignKey(to=Room, on_delete=models.PROTECT, blank=True, null=True, related_name='+')
-    room_confirmed = models.BooleanField(default=False, blank=True, null=True)
-    is_native_born = models.BooleanField(default=True, blank=False, null=False)
-    is_paying = models.BooleanField(default=False, blank=False, null=False)
-    father_name = models.CharField(max_length=32, blank=True, null=True)
-    id_card_number = models.SmallIntegerField(blank=True, null=True)
-    phone_number = models.CharField(max_length=16, blank=True, null=True)
-    is_early_bird = models.BooleanField(default=False, blank=False, null=False)
-    grade = models.CharField(max_length=16, blank=False, null=False, default='کارشناسی', choices=USER_GRADE_OPTIONS)
+    registration_date = models.DateTimeField(blank=True, null=True, auto_now_add=True, verbose_name='زمان ثبت نام')
+    user_instance = models.ForeignKey(to=User, on_delete=models.PROTECT, blank=False, null=False, related_name='+',
+                                      verbose_name='کاربر')
+    student_number = models.CharField(max_length=64, blank=False, null=False, verbose_name='شماره دانشجویی')
+    sex = models.BooleanField(default=False, blank=True, null=True,
+                              verbose_name='جنسیت')  # True for male, False for female
+    room = models.ForeignKey(to=Room, on_delete=models.PROTECT, blank=True, null=True, related_name='+',
+                             verbose_name='اتاق مورد نظر')
+    room_confirmed = models.BooleanField(default=False, blank=True, null=True, verbose_name='وضعیت تاییدیه اتاق')
+    is_native_born = models.BooleanField(default=True, blank=False, null=False, verbose_name='بومی یا غیر بومی')
+    is_paying = models.BooleanField(default=False, blank=False, null=False, verbose_name='شبانه یا روزانه')
+    father_name = models.CharField(max_length=32, blank=True, null=True, verbose_name='نام پدر')
+    id_card_number = models.SmallIntegerField(blank=True, null=True, verbose_name='کد ملی')
+    phone_number = models.CharField(max_length=16, blank=True, null=True, verbose_name='شماره تماس')
+    is_early_bird = models.BooleanField(default=False, blank=False, null=False, verbose_name='سحر خیز بودن')
+    grade = models.CharField(max_length=16, blank=False, null=False, default='کارشناسی', choices=USER_GRADE_OPTIONS,
+                             verbose_name='مقطع تحصیلی')
     requested_roommate = models.ForeignKey(to=User, on_delete=models.PROTECT, blank=True, null=True, default=None,
-                                           related_name='+')
-    is_noisy = models.BooleanField(default=False, blank=False, null=False)
+                                           related_name='+', verbose_name='هم اتاقی مورد نظر')
+    is_noisy = models.BooleanField(default=False, blank=False, null=False, verbose_name='حساس به سر صدا')
 
     def __str__(self):
         return f'{self.student_number}'
+
+    class Meta:
+        verbose_name = "پروفایل گاربری"
+        verbose_name_plural = "پروفایل های گاربری"
