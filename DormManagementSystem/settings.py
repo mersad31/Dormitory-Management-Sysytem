7f3eabb9d23f03adc690b58cb00b880c5d84c26c
Ali Akhtari <Aliakhtari78@hotmail.com>
2023-01-03 08:32:50 +0330

    Ready to deploy

--- DormManagementSystem/settings.py
+++ DormManagementSystem/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/4.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 from django.contrib import messages
@@ -115,11 +115,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 
-STATICFILES_DIRS = [BASE_DIR / "StaticFiles"]
-if DEBUG:
-    STATIC_URL = r'/StaticFiles/'
-else:
-    STATIC_ROOT = r'/StaticFiles/'
+STATIC_URL = r'/StaticFiles/'
+STATIC_ROOT = os.path.join(BASE_DIR, r'StaticFiles')
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
