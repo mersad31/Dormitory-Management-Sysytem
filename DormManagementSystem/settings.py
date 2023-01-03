054e0f4c5b543dc083db0151d11310b4099016f7
Ali Akhtari <Aliakhtari78@hotmail.com>
2023-01-03 08:30:17 +0330

    Ready to deploy

--- DormManagementSystem/settings.py
+++ DormManagementSystem/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-t@0lvw-$g-5)$j$)mbp$uc%@ju+ljtn^!&x5f%baw@m)uca=#a'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-CSRF_TRUSTED_ORIGINS = ['https://*.mr2mato.cloud', 'https://dormitory.mr2mato.cloud','https://*']
+CSRF_TRUSTED_ORIGINS = ['https://*.mr2mato.cloud', 'https://dormitory.mr2mato.cloud', 'https://*']
 ALLOWED_HOSTS = ['*', '127.0.0.1', 'dormitory.mr2mato.cloud']
 # Application definition
 
@@ -115,9 +115,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 
-STATIC_URL = r'/StaticFiles/'
 STATICFILES_DIRS = [BASE_DIR / "StaticFiles"]
-STATIC_ROOT = r'/StaticFiles/'
+if DEBUG:
+    STATIC_URL = r'/StaticFiles/'
+else:
+    STATIC_ROOT = r'/StaticFiles/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
