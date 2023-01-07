6b67e16bcfd989905c1fb9f663382d41d9dc34cc
Ali Akhtari <Aliakhtari78@hotmail.com>
2023-01-07 09:20:35 +0330

    Ready to deploy

--- DormManagementSystem/settings.py
+++ DormManagementSystem/settings.py
@@ -120,7 +120,9 @@ USE_TZ = True
 
 STATIC_URL = r'/StaticFiles/'
 STATIC_ROOT = os.path.join(BASE_DIR, r'StaticFiles')
-
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, r'/StaticFiles/'),
+]
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
